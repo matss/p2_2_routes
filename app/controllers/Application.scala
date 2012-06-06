f54f9d67f3631cbfa9058a3816bea0140d141e60
Mats Strandberg <mats@strandberg.net>
2012-06-06 14:46:52 +0200

    initial

---
+++ app/controllers/Application.scala
@@ -0,0 +1,18 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+
+object Application extends Controller {
+  
+  val idMap = Map[Int, String](1 -> "Steve", 2 -> "Bill").withDefaultValue("Unknown Id")
+  
+  def index(name : String = "Guest") = Action {
+    Ok(views.html.index(name))
+  }
+  
+  def indexId(id : Int) = Action {
+    Ok(views.html.index(idMap(id)))
+  }
+  
+}
